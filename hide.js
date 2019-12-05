3ff1850c65ed192e464e0ecc2277a6c93678602b
Naor Malka <malknaor@gmail.com>
2019-12-05 23:23:39 +0200

    Added credentials

---
+++ hide.js
@@ -0,0 +1,9 @@
+const CLIENT_ID = "8d711456c2054c028a2dea42e8c51140"; 
+const CLIENT_SECRET = "9859db8b83724d42b87fcfab2f13993b"; 
+const REDIRECT_URI = "http://127.0.0.1:8888/token"; 
+
+module.exports= {
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
+};
